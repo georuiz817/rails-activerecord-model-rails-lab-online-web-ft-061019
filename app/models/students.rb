34d101a41094c6518cc321e0443093613c846a5f
George Ruiz <georuiz817@gmail.com>
2019-08-25 17:13:08 +0000

    Automatically backed up by Learn

--- app/models/students.rb
+++ app/models/students.rb
@@ -1,5 +1,12 @@
 class Students < ActiveRecord::Base 
+      def initialize
+   @first_name = first_name
+   @last_name = last_name
+  end
+    
+    
+    
     def to_s
-   "first_name: #{@name}"
+   "first_name: #{@first_name},last_name: #{@last_name}"
   end
 end
